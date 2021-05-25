3b7fa95210bf35fba15d893bcdc88c961ffcc4d7
Yeser Amer <yamer@redhat.com>
2021-05-25 16:43:11 +0200

    KOGITO-5144: Create component structure

--- kie-wb-common-react/import-java-classes/showcase/src/index.tsx
+++ kie-wb-common-react/import-java-classes/showcase/src/index.tsx
@@ -2,10 +2,8 @@ import "@patternfly/react-core/dist/styles/base.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './GWTBridge';
-import { ImportJavaClasses } from "./components/ImportJavaClasses/ImportJavaClasses";
 
 ReactDOM.render(
-  <ImportJavaClasses buttonStyle="secondary" />,
+  <></>,
   document.getElementById('root')
 );
