d999f09a205ffa1dfc8b05d656ccb8665e1d7561
vpellegrino <vpellegr@redhat.com>
2021-06-14 15:56:11 +0200

    Introducing concept of groupType for a column in order to mark it differently in the DOM (className)

--- kie-wb-common-react/boxed-expression-editor/src/components/DecisionTableExpression/DecisionTableExpression.tsx
+++ kie-wb-common-react/boxed-expression-editor/src/components/DecisionTableExpression/DecisionTableExpression.tsx
@@ -14,9 +14,92 @@
  * limitations under the License.
  */
 
-import { DecisionTableProps } from "../../api";
+import "./DecisionTableExpression.css";
+import { BuiltinAggregation, DataType, DecisionTableProps, HitPolicy } from "../../api";
 import * as React from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { Table } from "../Table";
+import { ColumnInstance } from "react-table";
+import { HitPolicySelector } from "./HitPolicySelector";
+import * as _ from "lodash";
 
-export const DecisionTableExpression: React.FunctionComponent<DecisionTableProps> = () => {
-  return <div>Decision Table</div>;
+enum DecisionTableColumnType {
+  InputClause = "input",
+  OutputClause = "output",
+  Annotation = "annotation",
+}
+
+export const DecisionTableExpression: React.FunctionComponent<DecisionTableProps> = ({
+  uid,
+  hitPolicy = HitPolicy.Unique,
+  aggregation = BuiltinAggregation["<None>"],
+  input = [{ name: "input-1", dataType: DataType.Undefined }],
+  output = [{ name: "output-1", dataType: DataType.Undefined }],
+  annotations = ["annotation-1"],
+}) => {
+  const [selectedHitPolicy, setSelectedHitPolicy] = useState(hitPolicy);
+  const [selectedAggregation, setSelectedAggregation] = useState(aggregation);
+
+  const onHitPolicySelect = useCallback((itemId: string) => setSelectedHitPolicy(itemId as HitPolicy), []);
+
+  const onBuiltInAggregatorSelect = useCallback(
+    (itemId) => setSelectedAggregation((BuiltinAggregation as never)[itemId]),
+    []
+  );
+
+  const evaluateColumns = () => {
+    const inputColumns = _.map(
+      input,
+      (inputClause) =>
+        ({
+          label: inputClause.name,
+          accessor: inputClause.name,
+          dataType: inputClause.dataType,
+          groupType: DecisionTableColumnType.InputClause,
+        } as ColumnInstance)
+    );
+    const outputColumns = _.map(
+      output,
+      (outputClause) =>
+        ({
+          label: outputClause.name,
+          accessor: outputClause.name,
+          dataType: outputClause.dataType,
+          groupType: DecisionTableColumnType.OutputClause,
+        } as ColumnInstance)
+    );
+    const annotationColumns = _.map(
+      annotations,
+      (annotation) =>
+        ({
+          label: annotation,
+          accessor: annotation,
+          groupType: DecisionTableColumnType.Annotation,
+        } as ColumnInstance)
+    );
+
+    return [...inputColumns, ...outputColumns, ...annotationColumns];
+  };
+
+  const columns = useRef<ColumnInstance[]>(evaluateColumns());
+
+  return (
+    <div className={`decision-table-expression ${uid}`}>
+      <Table
+        columns={columns.current}
+        rows={[{}]}
+        controllerCell={useMemo(
+          () => (
+            <HitPolicySelector
+              selectedHitPolicy={selectedHitPolicy}
+              selectedBuiltInAggregator={selectedAggregation}
+              onHitPolicySelect={onHitPolicySelect}
+              onBuiltInAggregatorSelect={onBuiltInAggregatorSelect}
+            />
+          ),
+          [onBuiltInAggregatorSelect, onHitPolicySelect, selectedAggregation, selectedHitPolicy]
+        )}
+      />
+    </div>
+  );
 };
