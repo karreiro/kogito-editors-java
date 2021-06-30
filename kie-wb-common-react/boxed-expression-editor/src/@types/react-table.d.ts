9ae880cb926e1a98bd169fe0455f3fc6b3b64276
vpellegrino <vpellegr@redhat.com>
2021-06-30 16:29:27 +0200

    When opening table handler on parent column (Decision Name), the columns operation should be performed on its children

--- kie-wb-common-react/boxed-expression-editor/src/@types/react-table.d.ts
+++ kie-wb-common-react/boxed-expression-editor/src/@types/react-table.d.ts
@@ -58,6 +58,8 @@ declare module "react-table" {
     accessor: string;
     /** Column group type */
     groupType?: string;
+    /** Used to indicate that column operation should be performed directly on column's children */
+    appendColumnsOnChildren?: boolean;
     /** Column additive css classes - appended as passed */
     cssClasses?: string;
     /** Column label */
